be33bae755db177c0a2f2a0521c5f1e76db74a97
frizbee <mail2andrewa@gmail.com>
2018-11-05 01:21:27 +0800

    cal date range generate week

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @date_range = Date.today.beginning_of_week..Date.today.end_of_week
+    @date_range = (Date.today.beginning_of_week..Date.today.end_of_week).to_a
   end
 
   def create
