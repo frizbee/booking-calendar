f853346b2decdfb5d2710213596d1437c85fca24
frizbee <mail2andrewa@gmail.com>
2018-11-05 05:19:35 +0800

    js event

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -10,11 +10,46 @@ $(document).on('turbolinks:load', function() {
       $(document.body).on('click', '[data-date]', function() {
         self.getModal($(this).attr("data-date"));
       });
+
+      $("#submit_btn").on('click', function() {
+        let valid = self.validate();
+        if(valid) {
+          console.log('valid');
+          let form = document.querySelector('#add_event_form');
+          Rails.fire(form, 'submit');
+        }else{
+          $(".error-text").show();
+        }
+      });
     },
 
     getModal: function(date) {
       $("#eventModal").modal('toggle');
       $("#event_begin_date").val(date);
+    },
+
+    validate: function() {
+      let valid = true;
+
+      try {
+        $("#add_event_form .required").each(function() {
+          let $group = $(this);
+          $group.removeClass('error');
+          let val = '';
+
+          val = $group.find('input').val();
+          console.log('valid', val);
+
+          if( !val || val.length <= 0) {
+            $group.addClass('error');
+            valid = false;
+          }
+        });
+      } catch (e) {
+        console.log("ERROR: ",e);
+      }
+      console.log('end valid', valid);
+      return valid;
     }
 
   }
