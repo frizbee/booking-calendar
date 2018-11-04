97100c920c1479a8e6c692ddddb60c6c585ca05e
frizbee <mail2andrewa@gmail.com>
2018-11-05 01:15:16 +0800

    specs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -18,9 +18,10 @@ RSpec.describe EventsController, type: :controller do
 
   describe "GET @events" do
     @events = Event.create(title: "Booked by John", begin_date: Date.today, end_date: Date.today + 3.day)
+    date_range = (Date.today.beginning_of_week..Date.today.end_of_week).to_a
     it "assign date_rage" do
       get :index
-      expect(assigns(:date_range)).to match_array([@events])
+      expect(assigns(:date_range)).to match_array(date_range)
     end
   end
 
