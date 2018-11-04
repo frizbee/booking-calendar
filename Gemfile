cceaff7c28e0f9fcc473c55005a79b39b6e78ef1
frizbee <mail2andrewa@gmail.com>
2018-11-05 03:51:44 +0800

    shulda matchers

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ end
 
 group :test do
   gem 'database_cleaner'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:event) { Event.create(title: "Booked by John", begin_date: Date.today, end_date: Date.today + 3.day) }
+
+  describe "Validation" do
+
+    before :each do
+      event
+    end
+
+    it { is_expected.to validate_presence_of(:title) }
+
+  end
 end
